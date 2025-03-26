cc87bfaca858dfdbe3c0d35706509f7e4f480b20
Tharun Atithya <tharunaadhi6@gmail.com>
2025-03-26 15:12:41 +0530

    Python basics

---
+++ Python Basics/Day-11 topics/arithmetic_operators.py
@@ -0,0 +1,21 @@
+x = 10
+y = 5
+
+add = x + y
+sub = x - y 
+div = x / y
+mul = x * y
+floor_div = x // y
+mod = x % y
+exp = x ** y
+
+print("Add-" , add)
+print("sub-" , sub)
+print("mul-" , mul)
+print("div-" , div)
+print("floor division-" , floor_div)
+print("modulo-", mod)
+print("Exponent-", exp)
+# using f-string 
+print(f"add-{add} sub-{sub}")
+
---
+++ Python Basics/Day-11 topics/type.py
@@ -0,0 +1,19 @@
+# using type()
+x = 5
+y = 10 
+
+add = x + y
+sub = x - y 
+div = x / y
+mul = x * y
+floor_div = x // y
+mod = x % y
+exp = x ** y
+
+print(type(add))
+print(type(sub))
+print(type(mul))
+print(type(div))
+print(type(floor_div))
+print(type(mod))
+print(type(exp))
