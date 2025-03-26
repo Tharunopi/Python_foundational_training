923a6766bc263ded5fd7a74a1aee6477154d2651
Tharun Atithya <tharunaadhi6@gmail.com>
2025-03-26 15:49:10 +0530

    format and type conversions

---
+++ Python Basics/Day-11 topics/format.py
@@ -0,0 +1,5 @@
+price = 100.536
+item = "speaker"
+qty = 5
+order = "order the item {0} of quantity {1} with price {2}"
+print(order.format(item, qty, price))
---
+++ Python Basics/Day-11 topics/type_conversion.py
@@ -0,0 +1,9 @@
+# type conversion
+
+a = int(10)
+b = str(a)
+c = int(b)
+
+print(type(a), a)
+print(type(b), b)
+print(type(c), c)
