754d8d47596c1496304ec0c5f0c79712eaa66cf4
Tharun Atithya <tharunaadhi6@gmail.com>
2025-03-28 10:50:16 +0530

    string and math functions

---
+++ Python Basics/Day-13 topics/number_functions.py
@@ -0,0 +1,13 @@
+import math
+
+# floor division 
+print(math.floor(5))
+
+# ceil division 
+print(math.ceil(5))
+
+# sqrt 
+print(math.sqrt(383729))
+
+# pi value
+print(math.pi)
---
+++ Python Basics/Day-13 topics/string_indexing.py
@@ -0,0 +1,10 @@
+# string indexing
+a = "Python"
+
+print(a[1])
+
+# string slicing 
+print(a[:-4])
+
+# string length
+print(len(a))
