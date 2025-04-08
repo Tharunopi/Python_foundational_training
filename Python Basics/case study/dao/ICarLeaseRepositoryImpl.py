864607187e3969775d6253da5d6cdd6493d6377b
Tharun Atithya <tharunaadhi6@gmail.com>
2025-04-08 14:22:29 +0530

    added try except block for every method for exception handling

--- Python Basics/case study/dao/ICarLeaseRepositoryImpl.py
+++ Python Basics/case study/dao/ICarLeaseRepositoryImpl.py
@@ -91,112 +91,148 @@ class ICarLeaseRepositoryImpl(ICarLeaseRepository):
     
     # Customer Management
     def addCustomer(self, customer):
-        cur = self.connection.cursor()
+        try:
+            cur = self.connection.cursor()
 
-        query = "INSERT INTO Customer_Table VALUES (?, ?, ?, ?, ?)"
-        values = (customer.customerID, customer.firstName, customer.lastName, customer.email, customer.phoneNumber)
+            query = "INSERT INTO Customer_Table VALUES (?, ?, ?, ?, ?)"
+            values = (customer.customerID, customer.firstName, customer.lastName, customer.email, customer.phoneNumber)
 
-        cur.execute(query, values)
-        self.connection.commit()
-        cur.close()
-        return True
+            cur.execute(query, values)
+            self.connection.commit()
+            cur.close()
+            return True
+
+        except Exception as e:
+            print(f"Error Adding Customer : {e}")
 
     def removeCustomer(self, customerID):
-        cur = self.connection.cursor()
+        try:
+            cur = self.connection.cursor()
 
-        query = f"DELETE FROM Customer_Table WHERE customerID = {customerID}"
+            query = f"DELETE FROM Customer_Table WHERE customerID = {customerID}"
 
-        cur.execute(query)
-        self.connection.commit()
+            cur.execute(query)
+            self.connection.commit()
 
-        cur.close()
-        return True
+            cur.close()
+            return True
+        
+        except Exception as e:
+            print(f"Error Removing Customer : {e}")
 
     def listCustomers(self):
-        cur = self.connection.cursor()
+        try:
+            cur = self.connection.cursor()
 
-        query = "SELECT * FROM Customer_Table"
+            query = "SELECT * FROM Customer_Table"
 
-        cur.execute(query)
-        result = cur.fetchall()
-        cur.close()
-        return result
+            cur.execute(query)
+            result = cur.fetchall()
+            cur.close()
+            return result
+        
+        except Exception as e:
+            print(f"Error Listing Customers : {e}")
     
     def findCustomerById(self, customerID):
-        cur = self.connection.cursor()
+        try:
+            cur = self.connection.cursor()
 
-        query = f"SELECT * FROM Customer_Table WHERE customerID = {customerID}"
+            query = f"SELECT * FROM Customer_Table WHERE customerID = {customerID}"
 
-        cur.execute(query)
-        result = cur.fetchall()[0]
-        customer = Customer(result[0], result[1], result[2], result[3], result[4])
-        return customer
+            cur.execute(query)
+            result = cur.fetchall()[0]
+            customer = Customer(result[0], result[1], result[2], result[3], result[4])
+            return customer
+        
+        except Exception as e:
+            print(f"Error Finding Customer by ID : {e}")
     
     # Lease Management
     def createLease(self, customerID, carID, startDate, endDate):
-        cur = self.connection.cursor()
+        try:
+            cur = self.connection.cursor()
 
-        date_1 = datetime.strptime(startDate, "%Y-%m-%d").date()
-        date_2 = datetime.strptime(endDate, "%Y-%m-%d").date()
-        difference = date_2 - date_1
+            date_1 = datetime.strptime(startDate, "%Y-%m-%d").date()
+            date_2 = datetime.strptime(endDate, "%Y-%m-%d").date()
+            difference = date_2 - date_1
 
-        if difference.days >= 30:
-            type = "MonthlyLease"
-        else:
-            type = "DailyLease"
-            
-        query_1 = "INSERT INTO Lease_Table VALUES (?, ?, ?, ?, ?)"
-        values = (carID, customerID, startDate, endDate, type) 
+            if difference.days >= 30:
+                type = "MonthlyLease"
+            else:
+                type = "DailyLease"
+                
+            query_1 = "INSERT INTO Lease_Table VALUES (?, ?, ?, ?, ?)"
+            values = (carID, customerID, startDate, endDate, type) 
 
-        cur.execute(query_1, values)
-        self.connection.commit()
-        query_2 = 'SELECT TOP 1 leaseID FROM Lease_Table ORDER BY leaseID DESC'
-        cur.execute(query_2)
-        leaseID = cur.fetchone()[0]
-        query_3 = f"UPDATE Vechile_Table SET status = 'notAvailable' WHERE vechileID = {carID}"
-        cur.execute(query_3)
-        cur.commit()
-        cur.close()
-        return True
+            cur.execute(query_1, values)
+            self.connection.commit()
+            query_2 = 'SELECT TOP 1 leaseID FROM Lease_Table ORDER BY leaseID DESC'
+            cur.execute(query_2)
+            leaseID = cur.fetchone()[0]
+            query_3 = f"UPDATE Vechile_Table SET status = 'notAvailable' WHERE vechileID = {carID}"
+            cur.execute(query_3)
+            cur.commit()
+            cur.close()
+            return True
+        
+        except Exception as e:
+            print(f"Error Creating Lease : {e}")
     
     def returnCar(self, leaseID):
-        cur = self.connection.cursor()
+        try:
+            cur = self.connection.cursor()
 
-        query = f"SELECT * FROM Lease_Table WHERE leaseID = {leaseID}"
+            query = f"SELECT * FROM Lease_Table WHERE leaseID = {leaseID}"
 
-        cur.execute(query)
-        result = cur.fetchall()
-        cur.close()
-        return result
+            cur.execute(query)
+            result = cur.fetchall()
+            cur.close()
+            return result
+        
+        except Exception as e:
+            print(f"Error Returning Car : {e}")
     
     def listActiveLeases(self):
-        cur = self.connection.cursor()
+        try:
+            cur = self.connection.cursor()
 
-        query = "SELECT leaseID from Lease_Table WHERE CONVERT(DATE, GETDATE()) BETWEEN startDate AND endDate"
+            query = "SELECT leaseID from Lease_Table WHERE CONVERT(DATE, GETDATE()) BETWEEN startDate AND endDate"
 
-        cur.execute(query)
-        result = cur.fetchall()
-        cur.close()
-        return result
+            cur.execute(query)
+            result = cur.fetchall()
+            cur.close()
+            return result
+        
+        except Exception as e:
+            print(f"Error Listing Active Leases : {e}")
     
     def listLeaseHistory(self):
-        cur = self.connection.cursor()
+        try:
+            cur = self.connection.cursor()
 
-        query = "SELECT * FROM Lease_Table"
+            query = "SELECT * FROM Lease_Table"
 
-        cur.execute(query)
-        result = cur.fetchall()
-        cur.close()
-        return result
+            cur.execute(query)
+            result = cur.fetchall()
+            cur.close()
+            return result
+        
+        except Exception as e:
+            print(f"Error Listing Lease History : {e}")
     
     # Payment Table
     def recordPayment(self, lease, amount):
-        cur = self.connection.cursor()
+        try:
+            cur = self.connection.cursor()
 
-        query = "INSERT INTO Payment_Table VALUES (?, CONVERT(DATE, GETDATE()), ?)"
-        values = (lease.leaseID, amount)
+            query = "INSERT INTO Payment_Table VALUES (?, CONVERT(DATE, GETDATE()), ?)"
+            values = (lease.leaseID, amount)
 
-        cur.execute(query, values)
-        self.connection.commit()
-        cur.close()
-        return True
+            cur.execute(query, values)
+            self.connection.commit()
+            cur.close()
+            return True
+        
+        except Exception as e:
+            print(f"Error Recoding Payment : {e}")
